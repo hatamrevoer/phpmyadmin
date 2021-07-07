ec07b098707b3fc9b1a270b03bdff2ae917bedb0
datacvriskypratama <data.cvriskypratama@gmail.com>
2021-07-07 23:01:38 +0800

    All Commit

--- config.sample.inc.php
+++ config.sample.inc.php
@@ -28,7 +28,7 @@ $i++;
 /* Authentication type */
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
-$cfg['Servers'][$i]['host@example.oc'] = 'localhost';
+$cfg['Servers'][$i]['host'] = 'localhost';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = true;
 
